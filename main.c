c823d6337f87dd94dae72cdeda67e6d10bd4ec6e
javlonbekNormurodov <javlonbek.normuradov@gmail.com>
2021-11-02 21:42:45 +0500

    add a source file

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // loading
+    char b = '*';
+    int sanoq = 0;
+    while(1){
+    for(int a = 1;a < 10; a++){
+    for(int i = 1; i < 100000000;i++){
+    }
+    printf("\n");
+         printf("%c ",b);
+    }
+    system("clear");
+    if(sanoq == 2){
+        printf("Loading yakunlandi! ");
+        break;
+        }
+    else{
+        sanoq += 1;
+        }
+    }
+}
